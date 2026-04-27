b9a7d167e178f5d5d828b21c22c2be4fb8745eec
Aadak9 <your@email.com>
2026-04-27 09:35:45 +0200

    s

--- Lab3/lab3.m
+++ Lab3/lab3.m
@@ -5,8 +5,8 @@ B1_center = -5e6;
 B2 = 10e6;
 B2_center = -2.5e6;
 fs = 30e6;
-omega1 = -fs*pi/3;
-omega2 = fs*pi/6;
+w1 = -fs*pi/3;
+w2 = fs*pi/6;
 A1 = 1;
 A2 = 1;
 M1 = 4;
@@ -23,13 +23,10 @@ g2 = rcosdesign(rolloff1, S2, M2, 'sqrt')/sqrt(M2);
 
 figure(1)
 
-subplot(2,1,1)
-freqz(g1, L1)
-title('Filter g1 Frequency Response')
-
-subplot(2,1,2)
-freqz(g2, L2)
-title('Filter g2 Frequency Response')
+freqz(g1, L1); 
+hold on;
+freqz(g2, L2);
+legend("g1", "g2");
 
 
 figure(2)
@@ -54,40 +51,128 @@ title('G2 downsampled by M2')
 S1 = 1:40;
 S2 = 1:40;
 
-cases = [1 1;     % case 1: A1=1,   A2=1
-         1 0.1;   % case 2: A1=1,   A2=0.1
-         0.1 1];  % case 3: A1=0.1, A2=1
+cases = [1 1;     
+         1 0.1;   
+         0.1 1];  
+
+SIDR1_all = zeros(3,40);
+SIDR2_all = zeros(3,40);
+error_x1_all = zeros(3,40);
+error_x2_all = zeros(3,40);
 
 for c = 1:size(cases,1)
     A1 = cases(c,1);
     A2 = cases(c,2);
 
     for Q = [4, 64]
+
         x1 = qammod(randi([0 Q-1], L1, 1)', Q);
         x2 = qammod(randi([0 Q-1], L2, 1)', Q);
 
         SIDR1 = zeros(1, length(S1));
         SIDR2 = zeros(1, length(S2));
 
+        error_x1 = zeros(1, length(S1));
+        error_x2 = zeros(1, length(S2));
+
         for S = S1
 
-            %In this equation, it is assumed that the delay through the filters is zero. In the
-            %implementation, there is always an integer-valued filter delay that has to be taken
-            %care of before the SIDR computations.Append zeros somewhere on
-            %the signal?
+            % Transmitter
+            g1 = rcosdesign(rolloff1, S, M1, 'sqrt')/sqrt(M1);
+            g2 = rcosdesign(rolloff1, S, M2, 'sqrt')/sqrt(M2);
+            H1 = M1*g1;
+            H2 = M2*g2;
 
-            %%Transmitter
+            x1_transmit = upsample(x1, M1);
+            x2_transmit = upsample(x2, M2);
 
-            %Design filter
-            %H*G
-            %Upsample
+            x1_transmit = conv(x1_transmit, H1);
+            x2_transmit = conv(x2_transmit, H2);
 
-            %Transmit as a complex number?
-            %
+            x1_transmit = x1_transmit .* A1;
+            x2_transmit = x2_transmit .* A2;
+
+            n1 = 1:length(x1_transmit);
+            n2 = 1:length(x2_transmit);
+
+            x1_transmit = x1_transmit .* exp(1j * n1 * w1 / fs);
+            x2_transmit = x2_transmit .* exp(1j * n2 * w2 / fs);
+
+            num_of_zeros = length(x2_transmit) - length(x1_transmit);
+            x1_transmit = [x1_transmit zeros(1, num_of_zeros)];
+            y_tx = x1_transmit + x2_transmit;
+
+            % Receiver
+            n = 1:length(y_tx);
+            x1_received = y_tx .* exp(-1j * n * w1 / fs);
+            x2_received = y_tx .* exp(-1j * n * w2 / fs);
+
+            x1_received = conv(x1_received, g1);
+            x2_received = conv(x2_received, g2);
+
+            x1_received = downsample(x1_received, M1);
+            x2_received = downsample(x2_received, M2);
+
+            x1_received = x1_received .* 1/A1;
+            x2_received = x2_received .* 1/A2;
+
+            x1_est = x1_received(S+1:end-S*2);
+            x2_est = x2_received(S+1:end-S);
+
+            SIDR1(S) = 10*log10(sum(abs(x1).^2)/sum(abs(x1_est - x1).^2));
+            SIDR2(S) = 10*log10(sum(abs(x2).^2)/sum(abs(x2_est - x2).^2));
+
+            error_x1(S) = length(find(qamdemod(x1, Q) - qamdemod(x1_est, Q)));
+            error_x2(S) = length(find(qamdemod(x2, Q) - qamdemod(x2_est, Q)));
 
         end
-    end
 
+        % Store result per case 
+        SIDR1_all(c,:) = SIDR1;
+        SIDR2_all(c,:) = SIDR2;
+        error_x1_all(c,:) = error_x1;
+        error_x2_all(c,:) = error_x2;
+
+
+        if (A1 == 1 && A2 == 1)
+            figure(4);
+        elseif (A1 == 1 && A2 == 0.1)
+            figure(6);
+        elseif (A1 == 0.1 && A2 == 1)
+            figure(8);
+        end
+
+        if Q == 4
+            p = 1;
+        else
+            p = 2;
+        end
+
+        subplot(2,1,p)
+        plot(S1, SIDR1); hold on
+        plot(S1, SIDR2)
+        xlabel('S')
+        ylabel('SIDR (dB)')
+        title(['A1=',num2str(A1),' A2=',num2str(A2),' Q=',num2str(Q)])
+        legend('x1','x2')
+
+        if (A1 == 1 && A2 == 1)
+            figure(5);
+        elseif (A1 == 1 && A2 == 0.1)
+            figure(7);
+        elseif (A1 == 0.1 && A2 == 1)
+            figure(9);
+        end
+
+        subplot(2,1,p)
+        plot(S1, error_x1); hold on
+        plot(S1, error_x2)
+        xlabel('S')
+        ylabel('Errors')
+        title(['A1=',num2str(A1),' A2=',num2str(A2),' Q=',num2str(Q)])
+        legend('x1','x2')
+
+    end
 end
 
 
